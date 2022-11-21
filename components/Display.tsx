de527eb3b45b8ddb18d4fa1fff4a7a921c76d9cc
sagarkhadka <khadkasagar109@gmail.com>
2022-11-21 16:01:30 +0545

    simple animation tweak

--- components/Display.tsx
+++ components/Display.tsx
@@ -12,47 +12,56 @@ const Display = () => {
   let box2 = useRef(null)
   let box3 = useRef(null)
   
-  let img = useRef(null)
+  let img1 = useRef(null)
+  let img2 = useRef(null)
+  let img3 = useRef(null)
 
   useEffect(() => {
     const tl = gsap.timeline()
-    tl.to([box1, box2, box3], {
-      duration: 2,
-      ease: 'power3.inOut',
-      delay: 2,
-      width: 0,
-      stagger: {
-        amount: 0.5,
-      }
+    let ctx = gsap.context(() => {
+      tl.to([box1, box2, box3], {
+        duration: .8,
+        ease: 'power1.out',
+        delay: 2.6,
+        width: 0,
+        stagger: {
+          amount: 0.20,
+        }
+      })
+      
+      gsap.from([img1, img2, img3], {
+        delay: 2.7,
+        duration: .8,
+        ease: 'power4.in',
+        scale: 1.2,
+        stagger: {
+          amount: 0.2
+        }
+      })
     })
 
-    gsap.fromTo(img, {
-      duration: 1,
-      delay: 4,
-      scale: 1.2,
-    },{
-      duration: 1,
-      delay: 4,
-      ease: 'power3.inOut',
-      scale: 1,
-    })
+    return () => ctx.revert()
   }, [])
   return (
     <>
       <section className={styles.display_section}>
         <div className={styles.display_card}>
           <div ref={(el: any) => (box1 = el)} className={styles.overlay} />
-          <div className={styles.img_wrapper}>
-            <Image src={ui} fill alt='' objectFit='cover' />
+          <div ref={(el: any) => (img1 = el)} className={styles.img_wrapper}>
+            <Image src={ui} fill alt='' style={{ objectFit: 'cover' }} />
           </div>
         </div>
         <div className={styles.display_card}>
           <div ref={(el: any) => (box2 = el)} className={styles.overlay} />
-          <Image src={front} fill alt='' objectFit='cover' />
+          <div ref={(el: any) => (img2 = el)} className={styles.img_wrapper}>
+            <Image src={front} fill alt='' style={{ objectFit: 'cover' }} />
+          </div>
         </div>
         <div className={styles.display_card}>
           <div ref={(el: any) => (box3 = el)} className={styles.overlay} />
-          <Image src={graphic} fill alt='' objectFit='cover' />
+          <div ref={(el: any) => (img3 = el)} className={styles.img_wrapper}>
+            <Image src={graphic} fill alt='' style={{ objectFit: 'cover' }} />
+          </div>
         </div>
       </section>
     </>
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import { useEffect } from 'react'
 import '../styles/globals.scss'
 import type { AppProps } from 'next/app'
+import { gsap } from 'gsap'
 
 export default function App({ Component, pageProps }: AppProps) {
   useEffect(() => {
     let vh = window.innerHeight * 0.01
     document.documentElement.style.setProperty('--vh', `${vh}px`)
     document.documentElement.style.setProperty('--dvh', `${vh}px`)
-  })
+
+    // prevents flashing
+    gsap.from("body", { css: { visibility: 'visible' }})
+  }, [])
   return <Component {...pageProps} />
 }
